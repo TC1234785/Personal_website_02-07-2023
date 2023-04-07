1a86bbf64845258cc637712e290c6a2b1bf8e469
Your Name <youremail@yourdomain.com>
2023-04-07 03:18:41 -0700

    Complete course array portion

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,39 +1,40 @@
-let Class1 = {
-    ID: "ACIT 1630",
-    Name: "Database Systems"
-};
+/* Lab 8*/
+const courseTitle = document.querySelectorAll('li a')
+const courseDate = document.querySelectorAll('li p')
+const courseArray = []
 
-let Class2 = {
-    ID: "ACIT 1620",
-    Name: "Fundamental Web Technologies"
-};
 
-let Class3 = {
-    ID: "ACIT 1515",
-    Name: "Scripting for IT"
-};
-
-let courseList = [Class1, Class2, Class3];
-
-let user = prompt("Please enter your 4-digit course code: ");
-let isEqual = false;
-do {    
-    for (let i = 0; i < courseList.length; i++) {
-        if (courseList[i].ID.includes(user)) {
-            isEqual = true
-            console.log(`Yes I am taking the course: ${courseList[i]["ID"]} - ${courseList[i]["Name"]}`)
-        ;}
-
-        else{
-            
-        }
+function createCourseArray(){
+    for (let i=0; i<courseListSearch.length; i++){
+        let courseObject = {code: courseTitle[i].textContent, date:courseDate[i*2].textContent }
+        courseArray.push(courseObject)        
     }
-} while (isNaN(user)== true && user.length!= 4)
-
-if (isEqual === false) {
-    courseList.push({
-        ID: user,
-        Name: null
-    });
-    console.log("Success")
 }
+createCourseArray()
+
+function findCourse(courseList){
+    let user = prompt("Please enter your 4-digit course code: ");
+    let isEqual = false;
+    do {    
+        for (let i = 0; i < courseList.length; i++) {
+            if (courseList[i].ID.includes(user)) {
+                isEqual = true
+                console.log(`Yes I am taking the course: ${courseList[i]["ID"]} - ${courseList[i]["Name"]}`)
+            ;}
+    
+            else{
+                
+            }
+        }
+    } while (isNaN(user)== true && user.length!= 4)
+    
+    if (isEqual === false) {
+        courseList.push({
+            ID: user,
+            Name: null
+        });
+        console.log("Success")
+    }
+    
+    }
+    
