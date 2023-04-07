fc60da8b2cd06919ca05375841e9849d8cf29ac2
Your Name <youremail@yourdomain.com>
2023-04-07 04:29:27 -0700

    Completed findCourse() function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,40 +1,52 @@
 /* Lab 8*/
+const courseListSearch = document.querySelectorAll('.coursesUL li')
 const courseTitle = document.querySelectorAll('li a')
 const courseDate = document.querySelectorAll('li p')
-const courseArray = []
-
 
 function createCourseArray(){
+    const courseArray = []
     for (let i=0; i<courseListSearch.length; i++){
         let courseObject = {code: courseTitle[i].textContent, date:courseDate[i*2].textContent }
-        courseArray.push(courseObject)        
+        courseArray.push(courseObject)
+             
     }
+    return courseArray
 }
-createCourseArray()
+
 
 function findCourse(courseList){
-    let user = prompt("Please enter your 4-digit course code: ");
-    let isEqual = false;
-    do {    
-        for (let i = 0; i < courseList.length; i++) {
-            if (courseList[i].ID.includes(user)) {
-                isEqual = true
-                console.log(`Yes I am taking the course: ${courseList[i]["ID"]} - ${courseList[i]["Name"]}`)
-            ;}
-    
-            else{
-                
-            }
+    let validInput = false;
+    let userInput = ""
+    do {
+        userInput = prompt("Please enter your 4-digit course code: ");
+        if(isNaN(userInput)== true || userInput.length!= 4){
+        console.log('Your input was invalid. Please enter a 4-digit numerical course code.')
         }
-    } while (isNaN(user)== true && user.length!= 4)
+        else {
+            validInput = true;
+        }
+    } while (validInput === false)
     
-    if (isEqual === false) {
-        courseList.push({
-            ID: user,
-            Name: null
-        });
-        console.log("Success")
+    for(let i=0; i<courseList.length; i++){
+        if (courseList[i]['code'].includes(userInput)){
+            courseListSearch[i].classList.add('changeBGGreen')
+        }
+    }
+
+    if (courseList.filter(courseCode => courseCode.code !== userInput)){
+        let newCourse = document.createElement('li')
+        let newTitle = document.createElement('a')
+        newTitle.innerText = userInput
+        let newDate = document.createElement('p')
+        newDate.innerText = 'Fall 2020'
+        let newDesc = document.createElement('p')
+        newDesc.innerText = 'N/A'
+        newCourse.appendChild(newTitle)
+        newCourse.appendChild(newDate)
+        newCourse.appendChild(newDesc)
+        document.querySelector('ul').appendChild(newCourse)
     }
     
-    }
-    
+}
+
+findCourse(createCourseArray())
